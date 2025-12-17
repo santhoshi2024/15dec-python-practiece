203baa448cc640797797487ca830e92688fc204c
santhoshi <santhoshi6april@gmail.com>
2025-12-17 17:39:04 +0530

    value

---
+++ data type.py
@@ -0,0 +1,18 @@
+#0b
+#0B
+#0x, 0X
+#0o , 0O
+fever=101
+fever2= 100.2
+print(type(fever))
+print(type(fever2))
+fever3=0O233
+fever4=0x123
+print(fever3)
+print(fever4)
+print(type(fever3))
+print(type(fever4))
+
+
+
+print("santhoshi\'s"  "documentation on python")
