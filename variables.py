bdb78f2fbc7e60007b110925040a834127f0a34b
santhoshi <santhoshi6april@gmail.com>
2025-12-17 16:03:50 +0530

    workspace

--- variables.py
+++ variables.py
@@ -2,4 +2,14 @@ name=input("enter your name")
 length=len(name)
 print(length)
 print("welcome to play space")
- 
+
+
+a=100
+b="santhoshi"
+print(a)
+print(b)
+#print(a + 'santhoshi')
+
+name=input('enter your age')
+b="santhoshi"
+print(name + b)
