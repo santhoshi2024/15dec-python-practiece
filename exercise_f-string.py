40cdc978ef65bb0d28d19303e9ba3c3f2fc69d33
santhoshi <santhoshi6april@gmail.com>
2025-12-19 16:10:21 +0530

    f-string

---
+++ exercise_f-string.py
@@ -0,0 +1,9 @@
+age=input("enter your age")
+
+years_remaining=80-int(age)
+
+remaining_days=years_remaining*365
+remaining_weeks=years_remaining*52
+remaining_months=years_remaining*12
+
+print(f"you have {remaining_days} days, {remaining_weeks} weeks, {remaining_months}months")
