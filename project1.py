aa09cd07e050bc348a95a5b7775c24bf3e834024
santhoshi <santhoshi6april@gmail.com>
2025-12-25 00:07:11 +0530

    code

--- project1.py
+++ project1.py
@@ -1,28 +1,28 @@
 # ROCK PAPER SCISSOR
 import random
 user_choice=int(input("Enter your choice 0 for Rock , 1 for paper , 2 for scissor"))
-computer_choice=random.randint(0,2)
 
-print("Computer Choice-")
-print(computer_choice)
 if user_choice >2:
     print("invalid input") 
-
-if user_choice == computer_choice:
-    print("it's a draw")
-if user_choice > computer_choice:
-    print("you lose")
-    
-if user_choice < computer_choice:
-    print("you win")
-  
-if computer_choice > user_choice:
-    print("you win")
-
-if user_choice == 0 and computer_choice == 2:
+else:
+  computer_choice=random.randint(0,2)
+  print("Computer Choice-")
+  print(computer_choice)
+  if user_choice == 0 and computer_choice == 2:
     print('you win')
 
-if user_choice == 2 and computer_choice == 0:
+  elif user_choice == 2 and computer_choice == 0:
     print("you lose")
+  elif user_choice == computer_choice:
+    print("it's a draw")
+  elif user_choice > computer_choice:
+    print("you win")
+    
+  elif user_choice < computer_choice:
+    print("you win")
   
+  elif computer_choice > user_choice:
+    print("you lose")
+
+
       
