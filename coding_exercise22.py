6543c0a9e62a33d071daae52e83b1064357ae7f8
santhoshi <santhoshi6april@gmail.com>
2025-12-24 18:43:34 +0530

    test

---
+++ coding_exercise22.py
@@ -0,0 +1,14 @@
+row1 = '😖', '😖' , '😖'
+row2 = '😖', '😖' , '😖'
+row3 = '😖', '😖' ,'😖'
+
+matrix = [row1,row2,row3]
+print(f"{row1}\n{row2}\n{row3}")
+position=input('Enter the postion where you u want to hide car')
+row_number=int(position[0])
+coloum_number=int(position[1])
+row_selected=matrix[row_number-1]
+row_selected = list(row_selected)
+row_selected[coloum_number-1] = "🚗"
+
+
---
+++ nested-list.py
@@ -0,0 +1,5 @@
+list1 = [10, 20,30,['mohan' , 'syam', 'kanna'],40,50]
+print(len(list1))
+print(list1[3:2])
+print(list1[3])
+print(list1[3][::1])
