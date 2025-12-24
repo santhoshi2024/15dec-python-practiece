4f1d96b16502b1da2f88b430a57925c83d36353f
santhoshi <santhoshi6april@gmail.com>
2025-12-24 15:29:13 +0530

    gold

--- gold exercise.py
+++ gold exercise.py
@@ -1,6 +1,4 @@
-gold_rate_2026= 12,300
-
-
+gold_rate = 12,300 
 earings=6
 ring=4
 bangles=40
@@ -9,37 +7,40 @@ necklace=24
 
 print('welcome')
 
-gold=input("DO U REALLY WANT TO BUY GOLD")
+gold=input("DO U WANT TO BUY GOLD yes/no")
+if gold == "no":
+    print("ok, no purchase")
+
+year = int(input('enter the year 2026,2027,2028'))
 if gold == "yes":
-   year=input("which year do u want to buy gold")
-elif year == "2026":
+  if year == 2026:
+    print("gold rate is 12,300")
     print("earings-6, ring-4, bangles-40, chain-20, necklace-24")
-elif year == "2027":
-     print("earings-6, ring-4, bangles-40, chain-20, necklace-24")
-elif year == "2028":
+  elif year == 2027:
+    gold_rate= 12,300
     print("earings-6, ring-4, bangles-40, chain-20, necklace-24")
-else:
-   print('TRY NEXT YEAR')
+  elif year == 2028:
+    gold_rate == 12,300
+    print("earings-6, ring-4, bangles-40, chain-20, necklace-24")
+else: 
+  print("ok, no purchase")           
    
-ACCORING_TO_WEIGHT=input("please enter ornaments weight")
+ACCORING_TO_WEIGHT= str(input("please enter ornaments weight"))
+gold_cost1=earings*gold_rate
+gold_cost2=necklace*gold_rate
+gold_cost3=chain*gold_rate
+gold_cost4=ring*gold_rate
+gold_cost5=bangles*gold_rate
+gold_cost = gold_cost1 + gold_cost2 + gold_cost3 + gold_cost4 + gold_cost5
 
-
-
-gold_cost1=earings*gold_rate_2026
-gold_cost2=necklace*gold_rate_2026 
-gold_cost3=chain*gold_rate_2026
-gold_cost4=ring*gold_rate_2026
-gold_cost5=bangles*gold_rate_2026
-
-print(f("only net costof gold{gold_cost}Rs.")) 
-
+print(f"only net costof gold{gold_cost}Rs.")
 #increases  by 6% every year 
-increase_value=12,300*6 /100
+increase_value=gold_rate*6 /100
 
 print("Would you like me calculate with GST")
 
-GST=int(gold_cost1*2/100)
-total_value=gold_cost1 + increase_value + GST
+GST=int(gold_cost*2/100)
+total_value=gold_cost + increase_value + GST
 
 
 
